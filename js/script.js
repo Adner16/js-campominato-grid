28293795b8be31ecb773bfd35a1bddfcad17ce49
Adner16 <giovanni.renda16@gmail.com>
2022-01-17 15:37:12 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('js ok')
+console.log('js ok');
+
+// 1 richiamare l'elemento del dom
+// 2 aggiungi griglia al click delgi elementi
+// 3 cambia il colore al click delle celle
+
+
+
+let grid = document.getElementById('grid');
+const easy = document.getElementById('facile');
+const medium = document.getElementById('medio');
+const hard = document.getElementById('difficile');
+
+easy.addEventListener('click', function createGrid (1,100);{
+    grid = document.createElement('div');
+    grid= className('grid');
+})
