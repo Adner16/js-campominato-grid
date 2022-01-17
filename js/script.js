5fedbcf2a226472205a37fe2d57bec9b3eb7d11a
Adner16 <giovanni.renda16@gmail.com>
2022-01-17 16:17:42 +0100

    third commit

--- js/script.js
+++ js/script.js
@@ -7,11 +7,25 @@ console.log('js ok');
 
 
 let grid = document.getElementById('grid');
-const easy = document.getElementById('facile');
-const medium = document.getElementById('medio');
-const hard = document.getElementById('difficile');
+const livello = document.getElementById('livello');
+const play = document.getElementById('btn');
 
-easy.addEventListener('click', function createGrid (1,100);{
+btn.addEventListener('click', function createGrid (){
+    const choise = livello.value;
+    if (choise.value == 'easy'){
+        for (i = 0; i > 100; i++){
+           const grid = document.createElement('div');
+           cell.className = 'cell';
+        }if else (choise.value == medium){
+            
+        } else {
+
+        }
+    }
+
+
     grid = document.createElement('div');
     grid= className('grid');
 })
+
+
